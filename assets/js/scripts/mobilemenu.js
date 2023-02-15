dc987590faa066159b6a66d3f6e565e8d61b5fa7
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2023-02-15 16:10:56 +0000

    15 feb

--- assets/js/scripts/mobilemenu.js
+++ assets/js/scripts/mobilemenu.js
@@ -6,6 +6,11 @@ export function mobilemenu() {
 		$("body").toggleClass("opensidemenu");
 		$(".js-mobilemenu").toggleClass("open");
 	});
+	$("body.menuopen main").on("click", function() {
+		$("body").removeClass("menuopen");
+		$("body").removeClass("opensidemenu");
+		$(".js-mobilemenu").removeClass("open");
+	});
 	$( ".taalcontent .nav .nav-item" ).each(function() {
 	  var language = $(this).find('div').text();
 	  var newlanguage = language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ' + language + ' ';
