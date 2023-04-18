99a5ea91eecd4985ad77e4bc0dd446c68827bef6
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2023-04-18 15:43:13 +0000

    feedback 18 apr

---
+++ taxonomy-dossier_tags.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.2
+ */
+
+$context = Timber::get_context();
+ 
+$context['term'] = new TimberTerm();
+
+$args = array(
+'post_type' => 'dossiers',
+'tax_query' => array(
+    array(
+        'taxonomy' => 'dossier_tags',
+        'field'    => 'slug',
+        'terms'    => $context['term']->slug,
+    ),
+),
+);
+
+$context['posts'] = new Timber\PostQuery( $args );
+
+Timber::render( 'taxonomy-dossier_tags.twig', $context );
